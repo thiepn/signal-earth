4972849128837ff1a673432b30d59e4a19b56176
thiepn <junsosmart@gmail.com>
2026-09-14 23:08:39 +0200

    style: include Dynamic Briefings 2.0 styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './ui/styles/global.css';
 import './ui/styles/phase19.css';
 import './ui/styles/intelligence.css';
 import './ui/styles/orbit2.css';
+import './ui/styles/phase23.css';
 import { registerSignalEarthServiceWorker } from './core/release/serviceWorker';
 
 const root = document.getElementById('root');
