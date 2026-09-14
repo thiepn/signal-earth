cd2c8621f51ea18a0ff7756b1d9d7eb7e333d4ae
thiepn <junsosmart@gmail.com>
2026-09-14 23:03:33 +0200

    feat: export dynamic briefing composer

--- src/features/briefings/index.ts
+++ src/features/briefings/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './briefings';
+export * from './dynamic';
 export * from './TourEngine';
 export * from './BriefingLauncher';
 export * from './BriefingOverlay';
